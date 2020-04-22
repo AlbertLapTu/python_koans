bfebe5f949beefe3febb9c09eac432aeed927ff1
Albert <itzalbert2@gmail.com>
2020-04-22 15:19:41 -0700

    List assignments ✔

--- python3/koans/about_list_assignments.py
+++ python3/koans/about_list_assignments.py
@@ -10,28 +10,30 @@ from runner.koan import *
 class AboutListAssignments(Koan):
     def test_non_parallel_assignment(self):
         names = ["John", "Smith"]
-        self.assertEqual(__, names)
-
+        self.assertEqual(['John', 'Smith'], names)
+    # Parallel assignments assign variables to destructured arrays
     def test_parallel_assignments(self):
         first_name, last_name = ["John", "Smith"]
-        self.assertEqual(__, first_name)
-        self.assertEqual(__, last_name)
-
+        self.assertEqual("John", first_name)
+        self.assertEqual("Smith", last_name)
+    
     def test_parallel_assignments_with_extra_values(self):
         title, *first_names, last_name = ["Sir", "Ricky", "Bobby", "Worthington"]
-        self.assertEqual(__, title)
-        self.assertEqual(__, first_names)
-        self.assertEqual(__, last_name)
+        self.assertEqual("Sir", title)
+        self.assertEqual(["Ricky", "Bobby"], first_names)
+        self.assertEqual("Worthington", last_name)
 
+    # Python parallel assignment can destructure a value from a data structure
     def test_parallel_assignments_with_sublists(self):
         first_name, last_name = [["Willie", "Rae"], "Johnson"]
-        self.assertEqual(__, first_name)
-        self.assertEqual(__, last_name)
-
+        self.assertEqual(["Willie", "Rae"], first_name)
+        self.assertEqual("Johnson", last_name)
+    
+    # Similar to ES6 one line swaps
     def test_swapping_with_parallel_assignment(self):
         first_name = "Roy"
         last_name = "Rob"
         first_name, last_name = last_name, first_name
-        self.assertEqual(__, first_name)
-        self.assertEqual(__, last_name)
+        self.assertEqual("Rob", first_name)
+        self.assertEqual("Roy", last_name)
 
